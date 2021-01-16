43f5ac5a0e97abf052b179b65bf0f3c40ea6e8eb
David <dfan1315@gmail.com>
2021-01-15 21:48:22 -0800

    Front End

---
+++ Vue/src/components/board.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="board">
+        <div v-for="index in 5" :key="index">
+            <chessSquare color= "{{index}}" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import chessSquare from './chessSquare.vue'
+
+export default {
+    name: 'board',
+    components: {
+        chessSquare
+        }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
