375c65b78d7d0027e08b17c32d15dec69a6ccad5
David <dfan1315@gmail.com>
2021-01-16 01:16:31 -0800

    rendering chest board

--- Vue/src/components/board.vue
+++ Vue/src/components/board.vue
@@ -1,36 +1,42 @@
 <template>
     <div class="board">
-        <div v-for="index in 5" :key="index">
-            <chessSquare color= "{{index}}" />
-        </div>
+        <span v-for="row in 8" :key="row">
+            <span v-for="col in 8" :key="col">
+                <span class="row">
+                    <span v-if="(row+col)%2==0">
+                        <img src="@/assets/bord.png" />
+                    </span>
+                    <span v-else>
+                        <img src="@/assets/word.jpg" />
+                    </span>
+                </span>
+            </span>
+        </span>
     </div>
 </template>
 
 <script>
-    import chessSquare from './chessSquare.vue'
 
 export default {
     name: 'board',
-    components: {
-        chessSquare
-        }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+<style>
+    .board {
+        display: grid;
+        grid-template-columns: repeat(8, minmax(4, 4fr));
+        max-width: 300rem;
+        margin: 5rem auto;
+        padding: 0 5rem;
+    }
+
+    .row img {
+        resize: 30%;
+        width: 4vw;
+        height: 4vw;
+        object-fit: cover;
+        crop: "fill";
+    }
 </style>
