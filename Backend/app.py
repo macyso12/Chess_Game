c29a0504ff05bfbfb7585e9dea509029dd71c5f1
Anshuman-UCSB <anshuman@ucsb.edu>
2021-01-16 16:41:51 -0800

    Game manager

--- Backend/app.py
+++ Backend/app.py
@@ -3,11 +3,11 @@ from gameManager import gameManager
 import flask
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-app.run()
+app.run(host = "localhost")
